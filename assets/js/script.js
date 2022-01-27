dd93ced7972418143f26520053721e0e6f5c1705
admakinh <admakinh@gmail.com>
2022-01-27 01:49:31 -0500

    completed all javascript site functions to standard, readme not complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Reference DOM Elements
 var timeDisplayEl = $('#currentDay');
 var timeBlockEl = $('.textarea');
+var saveBtn = $('.saveBtn');
 
 
 // Display the current date
@@ -16,12 +17,12 @@ timeBlockEl.addClass(function (index) {
 
 var currentHour = moment().format('HH');
 
+$('.time-' + currentHour).toggleClass('present', true).toggleClass('past', false).toggleClass('future', false);
 
 var pastHour = currentHour;
 pastHour--;
 
 while (pastHour >= 9) {
-    console.log(pastHour);
     $('.time-' + pastHour).toggleClass('present', false).toggleClass('past', true).toggleClass('future', false);
     pastHour--;
 }
@@ -29,36 +30,32 @@ while (pastHour >= 9) {
 var futureHour = currentHour;
 futureHour++;
 while (futureHour <= 24) {
-    console.log(futureHour);
     $('.time-' + futureHour).toggleClass('present', false).toggleClass('past', false).toggleClass('future', true);
     futureHour++;
 }
 
+saveBtn.click(function () {
+    eventText = $(this).siblings('.input').val();
+    eventTime = $(this).siblings(".hour").text();
+    localStorage.setItem(eventTime, JSON.stringify(eventText));
+
+    loadText();
+});
 
 
+function loadText() {
+    var loopingNum = 9;
+    var timeLoop = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+    var loopLength = timeLoop.length;
+    for (var i = 0; i < loopLength; i++) {
+        var getEventText = JSON.parse(localStorage.getItem(timeLoop[i]));
+        $('.time-' + loopingNum).val("");
+        $('.time-' + loopingNum).val(getEventText);
 
-$('.time-' + currentHour).toggleClass('present', true).toggleClass('past', false).toggleClass('future', false);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        loopingNum++;
+    }
+}
 
 // display time each second
+loadText();
 setInterval(displayTime, 1000);
